f3157fcbab6fcc8afe718c25fe57844cbe75c446
Ryan Sapp <sappryam@gmail.com>
2016-09-14 18:41:10 -0400

    Homework 3 Module 1 All Problems Solved

--- m1-w1d3-expressions-and-control-flow/src/main/java/com/techelevator/Exercises.java
+++ m1-w1d3-expressions-and-control-flow/src/main/java/com/techelevator/Exercises.java
@@ -114,12 +114,17 @@ public class Exercises {
 	 posNeg(-4, -5, true) → true
 	 */
 	public boolean posNeg(int a, int b, boolean negative) {
-		if  (   (a < 0 && b > 0)   || (a > 0 && b < 0)){
-			return true;
-		} else if ((a < 0 && b < 0)  && negative) {
-			return true;
-		} else {
-		return false;
+		if  (negative){
+				return (a < 0 && b < 0);
+		}
+		else { 
+			return (a < 0 && b > 0 || a > 0 && b < 0);
+		//(   (a < 0 && b > 0)   || (a > 0 && b < 0)){
+		//	return true;
+		//} else if ((a < 0 && b < 0)  && negative) {
+			//return true;
+		//} else {
+		//return false;
 		}
 	}
 
