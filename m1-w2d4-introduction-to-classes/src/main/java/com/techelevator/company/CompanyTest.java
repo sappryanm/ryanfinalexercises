34dd60f5b2852370ee3e7d9bacd27b2e0dd4b98f
Ryan Sapp <sappryam@gmail.com>
2016-09-23 13:39:58 -0400

    Test on Company Complete but could be a better test

--- m1-w2d4-introduction-to-classes/src/main/java/com/techelevator/company/CompanyTest.java
+++ m1-w2d4-introduction-to-classes/src/main/java/com/techelevator/company/CompanyTest.java
@@ -5,23 +5,26 @@ package com.techelevator.company;
 public class CompanyTest {
 
 	public static void main(String[] args) {
-		
+		boolean allTestsPassed = true;
 		// write code here that verifies the functionality of the Company class
 		
-			
-			boolean allTestsPassed = true;
-			
-			/*** totalNumberOfItems should start at zero ***/
-			 Company name = new name();
-			if(Company.getName() = "") {
-				allTestsPassed = false;
-				System.out.println("Test FAILED: .");
-			}
-			
-							if(allTestsPassed) {
-									System.out.println("All tests PASSED!");
-				}
+		Company Company = new Company();
+		String name = "New Company";
+		Company.setName(name);
+		if(Company.getName() == null) {
+		allTestsPassed = false;
+		System.out.println("Test FAILED: No name found.");
+		} else if (!name.equals(Company.getName())){
+		allTestsPassed = false;
+		System.out.println("Test FAILED: Names did not match");
+		}
+
+		    
+		    
+		if(allTestsPassed){
+		System.out.println("All Test Passed");
+
 			}
 	}
 
-
+	}
