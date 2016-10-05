a9d351d8503ecb26240bb99043750b494a10837f
Ryan Sapp <sappryam@gmail.com>
2016-10-05 00:00:09 -0400

    Week 4 Day 2 done word search done through direct file name... Wordfineder is being used for testing differnt file i/o

--- m1-w4d2-file-io-part-1/src/main/java/com/techelevator/WordFinder.java
+++ m1-w4d2-file-io-part-1/src/main/java/com/techelevator/WordFinder.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,16 +8,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class WordSearch {
-//   Currently running on WordFinder.java but not wordscearh
+
+public class WordFinder {
 	  public static void main(String[] args) throws Exception {
 	        BufferedReader readerUser = new BufferedReader(new InputStreamReader(System.in));
 	        System.out.println("What would you like to search for? ");
 	        String userInput = readerUser.readLine();
 	        
-	       File file = new File("/Users/rsapp/Development/workspaces/ryansapp-java-exercises/"
-	       		+ "m1-w4d2-file-io-part-1/src/main/resources/alices_adventures_in_wonderland.txt");
-	        //File file = new File("./alices_adventures_in_wonderland.txt");
+	       File file = new File("http://www.textfiles.com/humor/TAGLINES/aphorism.txt");
 	        BufferedReader fileReader = new BufferedReader(new FileReader(file));
 
 	        String line = null;
@@ -31,5 +30,7 @@ public class WordSearch {
 
 	        fileReader.close();
 	    }
+	
+	
 
 }
