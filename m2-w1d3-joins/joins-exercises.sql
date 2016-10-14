498fe17985c6c5492d0e5bdeb6adea0544e6b664
Ryan Sapp <sappryam@gmail.com>
2016-10-14 11:31:23 -0400

    Commit before downloading Fridays homework

--- m2-w1d3-joins/joins-exercises.sql
+++ m2-w1d3-joins/joins-exercises.sql
@@ -27,31 +27,78 @@ INNER JOIN film_actor fa
 ON film.film_id = fa.film_id
 INNER JOIN actor
 ON fa.actor_id = actor.actor_id
-WHERE actor_id =(
-	SELECT actor_id
-	FROM actor
-	WHERE first_name = 'NICK' AND last_name = 'STALLONE'));
+WHERE actor_id =c);
 
 -- 2. All of the films that Rita Reynolds has appeared in
 --    Rows: 20
-
+SELECT title
+FROM film
+WHERE film_id IN (
+	SELECT film_id
+	FROM film_actor
+	WHERE actor_id =(
+		SELECT actor_id
+		FROM actor
+		WHERE first_name = 'RITA' AND last_name = 'REYNOLDS'));
 -- 3. All of the films that Judy Dean or River Dean have appeared in
 --    Rows: 46
-
+SELECT title
+FROM film
+WHERE film_id IN (
+SELECT film_id
+FROM film_actor
+WHERE actor_id = (
+SELECT actor_id
+FROM actor
+WHERE first_name = 'JUDY' AND last_name = 'DEAN') OR actor_id = (
+SELECT actor_id
+FROM actor
+WHERE first_name = 'RIVER' AND last_name = 'DEAN'));
 -- 4. All of the the ‘Documentary’ films
 --    Rows: 68
-
+SELECT title
+FROM film
+INNER JOIN film_category fc
+ON film.film_id = fc.film_id
+INNER JOIN category
+ON fc.category_id = category.category_id
+WHERE category.name = 'Documentary';
 -- 5. All of the ‘Comedy’ films
 --    Rows: 58
-
+SELECT title
+FROM film
+INNER JOIN film_category fc
+ON film.film_id = fc.film_id
+INNER JOIN category
+ON fc.category_id = category.category_id
+WHERE category.name = 'Comedy';
 -- 6. All of the ‘Children’ films that are rated ‘G’
 --    Rows: 10 
-
+SELECT title
+FROM film
+INNER JOIN film_category fc
+ON film.film_id = fc.film_id
+INNER JOIN category
+ON fc.category_id = category.category_id
+WHERE category.name = 'Children' AND film.rating = 'G';
 -- 7. All of the ‘Family’ films that are rated ‘G’ and are less than 2 hours in length
 --    Rows: 3
-
+SELECT title
+FROM film
+INNER JOIN film_category fc
+ON film.film_id = fc.film_id
+INNER JOIN category
+ON fc.category_id = category.category_id
+WHERE category.name = 'Family' AND film.rating = 'G' AND film.length <120;
 -- 8. All of the films featuring actor Matthew Leigh that are rated ‘G’
 --    Rows: 9
+SELECT title
+FROM film
+INNER JOIN film_actor fa
+ON film.film_id = fa.film_id
+INNER JOIN actor
+ON fa.actor_id = actor.actor_id
+WHERE actor.first_name = 'MATHEW' AND actor.last_name = 'LEIGH' AND film.rating = 'G';
 
 -- 9. All of the ‘Sci-Fi’ films released in 2006
 --    Rows: 61
