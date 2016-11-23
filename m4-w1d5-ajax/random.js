8e7097670a0b9b457ce950ced688b38ec277581a
Ryan Sapp <sappryam@gmail.com>
2016-11-22 21:47:41 -0500

    First API exercise done from work I missed Friday

---
+++ m4-w1d5-ajax/random.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+
+   var user = $("#results").val();
+  var items = [];
+   $.ajax({
+       url: "https://randomuser.me/api/?results=10",
+       type: "GET",
+       dataType: "json"
+   }).done(function (data) {
+       console.log(data);
+		
+       $("img").attr("src",data.results[0].picture.medium);
+		$("#fullName").append(data.results[0].name.first);
+		$("#fullName").append(", ");
+		$("#fullName").append(data.results[0].name.last);
+		$("#dob").append(data.results[0].dob);
+		$("#joined").append(data.results[0].registered);
+		$("#phone").append(data.results[0].phone);
+		$("#address").append(data.results[0].location.street + "   " + data.results[0].location.city + ", " + data.results[0].location.state + " " + 
+		data.results[0].location.postcode);
+		$("#email").append(data.results[0].email);
+		$("#username").append(data.results[0].login.username);
+		$("#password").append(data.results[0].login.password);
+		
+		$("#rotherUsers").empty();	
+       for (var i = 1; i < 10; i++) {
+			//var smallImage = $("img").attr("src",data.results[i].picture.thumbnail);
+          var tableRow = $("<tr>");
+			var pictureCell = $('<td>').prepend($('<img>',{id:'theImg',src: data.results[i].picture.thumbnail}))
+
+			var nameCellFirst = $("<td>").text( data.results[i].name.first);
+			var emailCell = $("<td>").text(data.results[i].email);
+			var phoneCell = $("<td>").text(data.results[i].phone);
+	 		
+           tableRow.append(pictureCell);
+           tableRow.append(nameCellFirst);
+			 tableRow.append(emailCell);
+			 tableRow.append(phoneCell);
+           
+
+           $("#otherUsers").append(tableRow);
+       }            
+   }).fail(function (xhr, status, error) {
+       console.log(error);
+   });
+
+    $("#refreshButton").on("click", function (event) {
+
+        var user = $("#results").val();
+		  var items = [];
+        $.ajax({
+            url: "https://randomuser.me/api/?results=10",
+            type: "GET",
+            dataType: "json"
+        }).done(function (data) {
+            console.log(data);
+				
+            $("img").attr("src",data.results[0].picture.medium);
+				$("#fullName").append(data.results[0].name.first);
+				$("#fullName").append(", ");
+				$("#fullName").append(data.results[0].name.last);
+				$("#dob").append(data.results[0].dob);
+				$("#joined").append(data.results[0].registered);
+				$("#phone").append(data.results[0].phone);
+				$("#address").append(data.results[0].location.street + "   " + data.results[0].location.city + ", " + data.results[0].location.state + " " + 
+				data.results[0].location.postcode);
+				$("#email").append(data.results[0].email);
+				$("#username").append(data.results[0].login.username);
+				$("#password").append(data.results[0].login.password);
+				
+				$("#rotherUsers").empty();	
+            for (var i = 1; i < 10; i++) {
+					//var smallImage = $("img").attr("src",data.results[i].picture.thumbnail);
+               var tableRow = $("<tr>");
+					var pictureCell = $('<td>').prepend($('<img>',{id:'theImg',src: data.results[i].picture.thumbnail}))
+
+					var nameCellFirst = $("<td>").text( data.results[i].name.first);
+					var emailCell = $("<td>").text(data.results[i].email);
+					var phoneCell = $("<td>").text(data.results[i].phone);
+			 		
+                tableRow.append(pictureCell);
+                tableRow.append(nameCellFirst);
+					 tableRow.append(emailCell);
+					 tableRow.append(phoneCell);
+                
+
+                $("#otherUsers").append(tableRow);
+            }            
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+
+   });
+
+});
